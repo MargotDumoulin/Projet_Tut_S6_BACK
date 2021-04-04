ca836ad79949c9e536b7e0d55c886324997cd5d0
Snorloux <margot.dumoulin18@gmail.com>
2021-04-04 22:30:11 +0200

    :card_file_box: Update tag scheme

--- src/Request/requestsTags.ts
+++ src/Request/requestsTags.ts
@@ -32,8 +32,8 @@ export const requestTags = (page: number) => {
     return {
         index: 'project_s6_tags',
         body: {
-            "from": ((page * 10) - 10),
-            "size": 10
+            "from": 0,
+            "size": 400
         }  
     }
 }
